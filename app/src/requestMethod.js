086b8fe3c9b286c78fd7a19e4ddab6c71ad43810
Nguyen Duy Tung <gtlduytung@gmail.com>
2022-03-22 23:13:46 +0700

    update header for verify function, update user model

--- app/src/requestMethod.js
+++ app/src/requestMethod.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzFmYmRjYTU2ZmNkOWE3MWI5NGJlMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzY3OTI1OCwiZXhwIjoxNjQ3NjgyODU4fQ.07X647RZ0RxhEuEVQM673Sbi6ymQpV-JEW5UVthkJLw";
+const userLocalStorage = localStorage.getItem('persist:root');
+let TOKEN = '';
+if (userLocalStorage) {
+    TOKEN = JSON.parse(JSON.parse(userLocalStorage)?.user)?.currentUser?.accessToken;
+}
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
